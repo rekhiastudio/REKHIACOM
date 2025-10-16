72fdd60a67f37ece77cd652a7c29fdd1f4a1cf42
rekhiastudio <233542741+rekhiastudio@users.noreply.github.com>
2025-10-16 18:52:14 +0300

    Hero Updated

--- components/ui/hero-parallax.tsx
+++ components/ui/hero-parallax.tsx
@@ -134,8 +134,8 @@ export const HeroParallax = ({ products }: { products: Product[] }) => {
 export const Header = () => {
   const t = useTranslations("Hero");
   return (
-    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
-      <h1 className="text-[1.7rem] md:text-6xl font-bold text-white">
+    <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 md:pl-20 w-full left-0 top-0 ">
+      <h1 className="text-[1.7rem] md:text-7xl font-bold text-white">
         {t.rich("title", {
           cl: (chunks) => <span className="text-red-500">{chunks}</span>,
           break: (chunks) => (
